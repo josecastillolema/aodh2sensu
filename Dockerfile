a24b7d5580197869ad92b3b07ee3bff9a1f20bd8
Cloud User <ec2-user@ip-172-31-62-106.ec2.internal>
2020-05-23 16:10:51 +0000

    sensu_url as parameter

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # FROM python:3
-FROM ubi8/python-38 
+FROM ubi8/python-36
 
 MAINTAINER Jose Castillo Lema <josecastillolema@gmail.com>
 
@@ -8,8 +8,9 @@ COPY aodh2sensu.py /opt/
 RUN pip3 install -r /tmp/requirements.txt
 
 ENV PORT=50000
+ENV SENSU_URL="localhost:4567"
 
 EXPOSE $PORT
 HEALTHCHECK CMD curl --fail http://localhost:$PORT || exit 1
 
-CMD [ "python3", "/opt/aodh2sensu.py" ]
+CMD python3 /opt/aodh2sensu.py --sensu-url $SENSU_URL
--- aodh2sensu.py
+++ aodh2sensu.py
@@ -1,13 +1,33 @@
 from flask import Flask, request
 import requests
 import json
+import sys
 # import logging
 
-app = Flask(__name__)
-sensu_url = 'http://localhost:4567/results'
-PORT=50000
+usage = """\
+Usage: aodh2sensu [options]
 
-@app.route("/", methods=['POST'])
+Imports OpenStack Aodh alarms into Sensu Core Server.
+
+Options:
+  -h, --help            show this help message and exit
+  --sensu-url URL       if not specified, defaults to localhost:4567"""
+sensu_url = 'localhost:4267'
+
+if (len(sys.argv) > 3) or\
+((len(sys.argv) == 3 and sys.argv[1] != '--sensu-url')) or\
+((len(sys.argv) == 2 and sys.argv[1] not in ['-h', '--help'])):
+   sys.exit(usage)
+elif len(sys.argv) == 2:
+   if (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
+      sys.exit(usage)
+elif ((len(sys.argv) == 3 and sys.argv[1] == '--sensu-url')):
+   sensu_url = sys.argv[2]
+
+PORT=50000
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
 def jenkins():
    aodh_alarm = json.loads(request.data)
    print (aodh_alarm)
@@ -18,39 +38,39 @@ def jenkins():
    else:
       status = -1
    sensu_alarm = {
-      "source": "aodh",
-      "name": aodh_alarm['alarm_name'],
-      "output": aodh_alarm['reason'],
-      "status": status
+      'source': 'aodh',
+      'name': aodh_alarm['alarm_name'],
+      'output': aodh_alarm['reason'],
+      'status': status
    }
    createSensuAlarm(sensu_alarm)
-   return "200 OK"
+   return '200 OK'
 
 def createSensuAlarm (sensu_alarm):
    body = sensu_alarm
    headers = {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
    }
    print (json.dumps(body))
-   r = requests.post(sensu_url, data=json.dumps(body), headers=headers)
+   r = requests.post('http://'+sensu_url+'/results', data=json.dumps(body), headers=headers)
 
    if r.status_code == 200 or r.status_code == 202:      
-      print("The resopnse code is " + r.status_code + ". Message sent succesfully")
-      # logging.info("The response code is 200. Message sent succesfully")
+      print('The response code is ' + r.status_code + '. Message sent succesfully')
+      # logging.info('The response code is 200. Message sent succesfully')
    elif r.status_code == 405:
-      print("The response code is 405. There was an error")
-      # logging.error("The response code is 405. There was an error")
+      print('The response code is 405. There was an error')
+      # logging.error('The response code is 405. There was an error')
    else:
-      print("The response code is " + str(r.status_code) + ". Unknown result")
-      #logging.error("The response code is " + str(r.status_code) + ". Unknown result")
+      print('The response code is ' + str(r.status_code) + '. Unknown result')
+      #logging.error('The response code is ' + str(r.status_code) + '. Unknown result')
 
    r.text_parsed = json.loads(r.text)
    print(json.dumps(r.text_parsed, indent=4, sort_keys=True))
    # logging.debug(json.dumps(r.text_parsed, indent=4, sort_keys=True))
 
-@app.route("/health", methods=['GET'])
+@app.route('/health', methods=['GET'])
 def health():
-   return "200 OK"
+   return '200 OK'
 
-if __name__ == "__main__":
-    app.run(port=PORT, host='0.0.0.0')
+if __name__ == '__main__':
+   app.run(port=PORT, host='0.0.0.0')
